08b0909a6c3a6f69040f8c836860d0e75560ba45
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 16:32:17 +0300

    Initial commit

---
+++ WorkingHours1/Program.cs
@@ -0,0 +1,21 @@
+﻿class WorkingHours{
+    public static void Main (string[]args) {
+        int hour=int. Parse(Console.ReadLine());
+        string day=Console.ReadLine();
+    
+        if (day=="Monday" || day=="Tuesday" || 
+            day=="Wednesday" ||day=="Thursday" ||day=="Friday" 
+            ||
+            day=="Saturday") {
+            if(hour>=10 && hour<=18) {
+                Console.WriteLine("open") ; 
+            } 
+            else {
+                Console.WriteLine("closed") ;
+            } 
+        } 
+        else if(day=="Sunday"){
+            Console.WriteLine ("closed") ;
+        } 
+    } 
+}
